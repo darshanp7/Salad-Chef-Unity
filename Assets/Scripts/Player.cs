fdbdcdede91bcfe371691246ca39fa8e0b9d3de8
darshan <darshanp7@hotmail.com>
2019-09-02 00:23:54 +0530

    Added HUD to Players and Customers

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float speed = 1000.0f;
+    
+    void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+         
+         Debug.Log("Speed " + speed);
+    }
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = Time.deltaTime * speed * movement;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+}
